6edf81d4e91a874ae7100024fa0edd9d193e266c
hazzymazzy <101456121+hazzymazzy@users.noreply.github.com>
2025-10-05 12:26:27 +1100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,6 @@ output "website_bucket_name" {
   description = "S3 bucket storing site content (private)"
 }
 
-# The public URL is now served by CloudFront
 output "cloudfront_url" {
   value       = "https://${aws_cloudfront_distribution.cdn.domain_name}"
   description = "Public HTTPS URL for the site"
